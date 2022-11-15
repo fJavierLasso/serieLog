a1556a393c36f7d1e9818374274a28284945ec4f
Javier Lasso <fjavierlasso@icloud.com>
2022-11-15 21:05:37 +0100

    comentarios en tablaSeries añadidos

--- tablaSeries.php
+++ tablaSeries.php
@@ -1,13 +1,8 @@
-<!-- Aquí imprimiremos los valores de clasesPadre/bbdd.txt en una tabla.
+<!-- Aquí imprimiremos los valores de bbdd.txt en una tabla.
 También tendremos un botón para volver a la página de series.
 
 Si nos sobra tiempo, añadiremos botones para eliminar series de esta lista.
 Y la función de que te avise si hay capítulo de serie el día de la semana.-->
-
-<?php
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="es">
