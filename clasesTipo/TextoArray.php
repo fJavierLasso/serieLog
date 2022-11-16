80920520c643ab1980a42fe998af46b8c40d5211
Javier Lasso <fjavierlasso@icloud.com>
2022-11-16 18:21:11 +0100

    rehaciendo TextoArray

--- clasesTipo/TextoArray.php
+++ clasesTipo/TextoArray.php
@@ -5,6 +5,8 @@ class TextoArray extends Atipo{
     protected $valor;
     protected $name;
     protected $error;
+    protected $atributos = array();
+    protected $select = false;
     private const DIAS = [
         "L",
         "M",
@@ -31,46 +33,65 @@ class TextoArray extends Atipo{
         "Suspense",
         "Fantasia"
     ];
-    public function __construct($valor, $name){
+    public function __construct($valor, $name, $atributos, $select){
         $this->valor=$valor;
         $this->name = $name;
+        if ($select) {
+            $this->select = true;
+        }
+        foreach ($atributos as $atributo) {
+            array_push($this->atributos,$atributo);
+        }
     }
     public function validarEspecifico($value){
-        if(is_array($value)){
-            if(empty(array_diff(self::GENEROS, $value))){
-                $this->error = "La opción seleccionada no existe";
-                return false;
-            }else{
-                $this->valor = $value;
-                return true;
-            }
-        }
-        else{
-            if (!(in_array($value, self::DIAS)) && !(in_array($value, self::PLATAFORMAS)) ){
-                $this->error="La opción seleccionada no existe";
-                return false;
-                }else {
-                    $this->valor = $value;
-                    return true;       
-                }
-            }
+       
+        return (in_array($value, $this->atributos));
+
+       
+       
+        // if(is_array($value)){
+        //     if(empty(array_diff(self::GENEROS, $value))){
+        //         $this->error = "La opción seleccionada no existe";
+        //         return false;
+        //     }else{
+        //         $this->valor = $value;
+        //         return true;
+        //     }
+        // }
+        // else{
+        //     if (!(in_array($value, self::DIAS)) && !(in_array($value, self::PLATAFORMAS)) ){
+        //         $this->error="La opción seleccionada no existe";
+        //         return false;
+        //         }else {
+        //             $this->valor = $value;
+        //             return true;       
+        //         }
+        //     }
     }
     public function pintar(){
-        switch ( strtoupper($this->name)) {
-            case 'GENEROS':
-                print '<div class="checkbox__generos">';
-                foreach (self::GENEROS as $key => $value) {
-                    
-                    print '<label for="'.$this->name.'"><input type="checkbox" id="'.$this->name.'" name="generos[]" value="'.$value.'">'. $value .'</label>';
-                }
-                print '</div>';
-                break;
-            case 'PLATAFORMAS':
-                $this->pintarDataList(self::PLATAFORMAS);break;
-            case 'DIAS':
-                $this->pintarDataList(self::DIAS);break;            
-            default:break;
+
+        if ($this->select) {
+            //pinto un select con this atributos
+        } else {
+            //pinto checkboxes con this atributos
         }
+
+
+        // switch ( strtoupper($this->name)) {
+        //     case 'GENEROS':
+        //         print '<div class="checkbox__generos">';
+        //         foreach (self::GENEROS as $key => $value) {
+                    
+        //             print '<label for="'.$this->name.'"><input type="checkbox" id="'.$this->name.'" name="generos[]" value="'.$value.'">'. $value .'</label>';
+        //         }
+        //         print '</div>';
+        //         break;
+        //     case 'PLATAFORMAS':
+        //         $this->pintarDataList(self::PLATAFORMAS);break;
+        //     case 'DIAS':
+        //         $this->pintarDataList(self::DIAS);break;            
+        //     default:break;
+        // }
     }
     public function pintarDataList($arr){
         print '<div>';
